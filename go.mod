4a7c92d305670d6bb142eb8b1127c8691b71477b
Kolin Fluence <kolinfluence@gmail.com>
2023-04-06 23:45:02 -0700

    123

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/cloudxaas/gorand
+
+go 1.20
+
+require (
+	github.com/bytedance/gopkg v0.0.0-20230324090325-a00d8057bef9
+	lukechampine.com/frand v1.4.2
+)
+
+require (
+	github.com/aead/chacha20 v0.0.0-20180709150244-8b13a72661da // indirect
+	golang.org/x/sys v0.0.0-20221010170243-090e33056c14 // indirect
+)
--- rand.go
+++ rand.go
@@ -1,50 +1,46 @@
 package cxrand
 
 import (
-    "unsafe"
-    "time"
-    crand "lukechampine.com/frand"
-    fastrand "github.com/bytedance/gopkg/lang/fastrand"
+	"crypto/rand"
+	"time"
+
+	fastrand "github.com/bytedance/gopkg/lang/fastrand"
 )
 
-//0a
-func RandomBytes(n int) []byte {
-    bytes := make([]byte, n)
-    var randNum uint32
-    for i := 0; i < n; i += 4 {
-        randNum = fastrand.Uint32()
-        if i+3 < n {
-            bytes[i] = byte(randNum)
-            bytes[i+1] = byte(randNum >> 8)
-            bytes[i+2] = byte(randNum >> 16)
-            bytes[i+3] = byte(randNum >> 24)
-        } else {
-            for j := 0; j < n-i; j++ {
-                bytes[i+j] = byte(randNum >> (8 * j))
-            }
-        }
-    }
-    return bytes
+func RandomBytes(b []byte) {
+	n := len(b)
+	var randNum uint32
+	for i := 0; i < n; i += 4 {
+		randNum = fastrand.Uint32()
+		if i+3 < n {
+			b[i] = byte(randNum)
+			b[i+1] = byte(randNum >> 8)
+			b[i+2] = byte(randNum >> 16)
+			b[i+3] = byte(randNum >> 24)
+		} else {
+			for j := 0; j < n-i; j++ {
+				b[i+j] = byte(randNum >> (8 * j))
+			}
+		}
+	}
 }
 
-//0a, cryptographic random bytes
-func CRandomBytes(n int) []byte {
-    bytes := make([]byte, n)
-    crand.Read(bytes)
-    return bytes
+func CRandomBytes(b []byte) {
+	_, err := rand.Read(b)
+	if err != nil {
+		panic(err)
+	}
 }
 
-//0a, random string
-func RandomString(length int) string {
-    b := make([]byte, length)
-    var t int64
-    for i := 0; i < length; i++ {
-        t = time.Now().UnixNano()
-        randByte := byte(t & 0xff)
-        if randByte < 32 || randByte > 126 {
-            randByte = 32
-        }
-        b[i] = randByte
-    }
-    return *(*string)(unsafe.Pointer(&b))
+func RandomString(b []byte) {
+	length := len(b)
+	var t int64
+	for i := 0; i < length; i++ {
+		t = time.Now().UnixNano()
+		randByte := byte(t & 0xff)
+		if randByte < 32 || randByte > 126 {
+			randByte = 32
+		}
+		b[i] = randByte
+	}
 }
